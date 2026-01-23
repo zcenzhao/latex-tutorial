65a08a53bb9ff2860fc336447f4264f9ea7c512b
zcenzhao <z2694506351@gmail.com>
2026-01-23 20:53:55 +0800

    update docs

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+/* 适配 Material 的 instant loading：每次切换页面都重新渲染 */
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
